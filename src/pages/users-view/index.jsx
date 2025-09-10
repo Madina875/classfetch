0814ba8f6f695fe0327397fcc4059dde3c21587f
madina875 <evaevasmq@gmail.com>
2025-09-10 13:45:13 +0500

    hmw

--- src/pages/users-view/index.jsx
+++ src/pages/users-view/index.jsx
@@ -4,9 +4,12 @@ const UsersView = ({ data }) => {
   return (
     <div className="w-[70%] h-[800px] bg-gray-200 overflow-scroll rounded-2xl grid grid-cols-3 gap-[30px]">
       {data?.map((e) => (
-        <div className="border h-[200px]" key={e.id}>
-          <h2>{e.name}</h2>
-          <strong>{e.age}</strong>
+        <div
+          className="border h-[200px] rounded-2xl border-gray-400 flex justify-center items-center flex-col gap-[20px]"
+          key={e.id}
+        >
+          <h2 className="text-4xl italic">name: {e.name}</h2>
+          <strong>age: {e.age}</strong>
         </div>
       ))}
     </div>
